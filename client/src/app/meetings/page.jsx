1d6cd6ffa619ce3df51b4ac33a0b440e609f3ba5
Preet Singh <Preetsingh921@gmail.com>
2026-08-13 17:22:38 +0530

    fix: import useMemo in meetings page

--- client/src/app/meetings/page.jsx
+++ client/src/app/meetings/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo, useRef, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import {
